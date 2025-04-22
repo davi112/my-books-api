164d846b55c8373694eda4da463140ecb3859d78
Davi Fonseca <davimartinsfreitas@gmail.com>
2025-04-21 22:59:37 -0300

    comments

--- src/index.js
+++ src/index.js
@@ -16,3 +16,5 @@ app.use((error, request, response, next) => {
 });
 
 app.listen(3000, () => console.log("Servidor iniciado em http://localhost:3000"));
+
+//docker compose down && docker rmi my-books-api-node-app:latest && docker compose up --build -d
